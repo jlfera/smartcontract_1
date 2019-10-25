19d711af73d6595990bf74896918ced3247f6a7e
Jose Luis <32198739+jlfera@users.noreply.github.com>
2019-10-24 22:23:20 -0300

    Add files via upload

---
+++ truffle.js
@@ -0,0 +1,25 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "spirit supply whale amount human item harsh scare congress discover talent hamster";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24"
+    }
+  },
+  rinkeby: {
+      provider: function() { 
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/1e91f037522e475abddee9b089c6f878");
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+  }
+};
