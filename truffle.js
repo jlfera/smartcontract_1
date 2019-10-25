736d7490315fe819682e3e2af4ccccb6d5e960b4
Jose Luis <32198739+jlfera@users.noreply.github.com>
2019-10-25 14:13:51 -0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,4 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
+//words od your account MetaMask
 var mnemonic = "spirit supply whale amount human item harsh scare congress discover talent hamster";
 
 module.exports = {
@@ -7,19 +8,19 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
-  },
-  compilers: {
-    solc: {
-      version: "0.4.24"
-    }
-  },
-  rinkeby: {
+    },
+    rinkeby: {
       provider: function() { 
        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/1e91f037522e475abddee9b089c6f878");
       },
       network_id: 4,
       gas: 4500000,
       gasPrice: 10000000000,
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24"
+    }
   }
 };
